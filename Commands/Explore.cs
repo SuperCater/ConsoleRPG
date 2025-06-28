f846275a100549ecf1d660f26479a95b01628c97
SuperCater <supercater500@gmail.com>
2025-06-28 06:35:29 -0400

    wip explore command

---
+++ Commands/Explore.cs
@@ -0,0 +1,14 @@
+using ConsoleRPG.Classes;
+using ConsoleRPG.Records;
+
+namespace ConsoleRPG.Commands;
+
+public class Explore() : Command(new CommandOptions
+{
+    Name = "explore",
+    Description = "Explore the world around you",
+    Action = (input) =>
+    {
+       return "WIP["
+    }
+});
